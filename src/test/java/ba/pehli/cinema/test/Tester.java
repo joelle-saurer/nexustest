49efee1f55c695c5c62f3f8cb41b34ac9306cbad
joelle-saurer <72863109+joelle-saurer@users.noreply.github.com>
2020-11-15 15:05:11 +0100

    Update Tester.java

--- src/test/java/ba/pehli/cinema/test/Tester.java
+++ src/test/java/ba/pehli/cinema/test/Tester.java
@@ -8,9 +8,7 @@ import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 import org.springframework.social.oauth1.OAuth1Operations;
 import org.springframework.social.oauth2.AccessGrant;
 import org.springframework.social.oauth2.OAuth2Operations;
-new import
-new content on newbranch
-nw content
+
 public class Tester {
 	
 
